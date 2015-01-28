8ac383022165f12ffb5f97bd9d1ae624a4b6637b
Nwdwn <fakkaman@gmail.com>
2015-01-28 13:42:07 +0100

    Update Menu.hx
    Comments added.

--- Lane Card Game Matthijs/src/Menu.hx
+++ Lane Card Game Matthijs/src/Menu.hx
@@ -11,6 +11,7 @@ import flash.system.System;
  */
 class Menu extends openfl.display.Sprite
 {
+	// Down below are are all the var's for the menu which declare the buttons and sound for the menu //
 
 	var musicvolume:Float = 1.0 ;
 	var menuMusic = new Music();
@@ -18,11 +19,17 @@ class Menu extends openfl.display.Sprite
 	var startbutton:Menubutton = new Menubutton("img/MenuButtonStart.png", "img/MenuButtonStartHover.png");
 	var exitbutton:Menubutton = new Menubutton("img/MenuButtonExit.png", "img/MenuButtonExitHover.png");
 	public var start:Bool = false;
+	
 
+	// public funtion new, this adds a new event to the stage, which is an event listener //
+
+	
 	public function new() 
 	{
 		super();
 		addEventListener(Event.ADDED_TO_STAGE, added);
+	
+	// function added, this draws the menu and background and declare these below  //
 
 	}
 
@@ -35,6 +42,9 @@ class Menu extends openfl.display.Sprite
 		playMusic();
 		
 	}
+	
+	// funtion draw menu, this actualy draws the menu, using a and y parameters// 
+	//openfl always calculates the position from the top left corner//
 	
 	function drawmenu() {
 
@@ -51,6 +61,8 @@ class Menu extends openfl.display.Sprite
 
 	}
 	
+	// fucntion startGame, this stops the game music, and gets the game in to the playstate. //
+	
 	function startGame(e:MouseEvent) 
 	{
 		//startbutton.removeEventListener(MouseEvent.CLICK, startGame);
@@ -59,16 +71,23 @@ class Menu extends openfl.display.Sprite
 		menuMusic.stopMusic ();
 		removeChild (menuMusic);
 	}
-
+	
+	// function exit, this shuts down the program and stops all code. //
+	
 	function exit(e:MouseEvent) 
 	{
 		System.exit(0);
 	}
-
+	
+	// function drawbackgound, this function called the var background and draws the Background.png file, because we did not state positioning
+	// x.y/0.0 its gets drawn 0.0 and used the full scale of the picture, which in this case is the size of the game.//
+	
 	function drawbackground() 
+	
 	{
 		var background = new Bitmap(Assets.getBitmapData("img/Background.png"));
 		addChildAt (background, 0);
+	
 		
 	}
 
