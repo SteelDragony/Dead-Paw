d262956b77ffaf56da3a05b3e440f220bc573e63
Nwdwn <fakkaman@gmail.com>
2015-01-28 14:22:28 +0100

    Update Menubutton.hx
    Added Comments.

--- Lane Card Game Matthijs/src/Menubutton.hx
+++ Lane Card Game Matthijs/src/Menubutton.hx
@@ -18,7 +18,8 @@ class Menubutton extends Sprite
 	var mainImageHover:Bitmap; 
 	
 	//public funtion new, this delcared that there are two images needed to initiate the the function HOVER// 
-	
+	// The eventlisters checks if the funtion is actialy is being executed, if mouse over, the menu hovers and calls
+	// the hover.png file. if not the regular .png file gets loaded.//
 	public function new(image:String, imageHover:String) 
 	{
 	
@@ -30,13 +31,23 @@ class Menubutton extends Sprite
 		addEventListener(MouseEvent.MOUSE_OVER, OnMouseOver);
 		addEventListener(MouseEvent.MOUSE_OUT, OnMouseOut);
 	}
+	
+	// The draw function draws the mainimage(declared in menu,hx) // 
+	
 	function draw(){
 		addChild(mainiamge);
+	
 	}
+	
+	// The Onmouse removes the main button and replace's the button with a hoverbutton //
+	
 	function OnMouseOver(e:MouseEvent) {
 		removeChildren();
 		addChild(mainImageHover);	
 	}	
+	
+	// When the mouse hover away from the button the child thats is called in the draw funtion is recalled//
+	
 	function OnMouseOut(e:MouseEvent) {
 		removeChildren();
 		addChild(mainiamge);
